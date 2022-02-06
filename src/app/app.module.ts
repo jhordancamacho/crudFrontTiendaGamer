301b0d32817887cfcce674dedba652d291aba582
jhordancamacho <jhordan.camacho@gmail.com>
2022-02-05 22:22:09 -0500

    add models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//importaciones externas
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { BuscarJuegoComponent } from './juegos/buscar-juego.component';
+import { AgregarJuegoComponent } from './juegos/agregar-juego.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarJuegoComponent,
+    AgregarJuegoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/juegos/agregar-juego.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-juego',
+  templateUrl: './agregar-juego.component.html',
+  styleUrls: ['./agregar-juego.component.css']
+})
+export class AgregarJuegoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
